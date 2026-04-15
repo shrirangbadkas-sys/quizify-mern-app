4d22bbe24f65a5f08e2f2ba29153116304356ce7
Shrirang_Badkas <shrirangbadkas@gmail.com>
2026-04-15 19:43:06 +0530

    final fix for deployment

--- server/index.js
+++ server/index.js
@@ -6,22 +6,26 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 app.use("/api/result", require("./routes/resultRoutes"));
 
-// MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/quizDB")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+// ✅ MongoDB Atlas connection
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: "quizify"
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
 // test route
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+// ✅ Dynamic port for Railway
+const PORT = process.env.PORT || 5000;
 
-
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
